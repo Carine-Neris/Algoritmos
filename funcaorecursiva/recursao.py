6175169304b3414219706b527cd531b2bbaaa660
Carine Neris <carine_neris@hotmail.com>
2025-04-22 11:10:04 -0300

    adicionado o parametro correto na função contagem regressiva

--- funcaorecursiva/recursao.py
+++ funcaorecursiva/recursao.py
@@ -1,14 +1,14 @@
 """ Recursão é quando uma função chama a si mesma  """
 
 # Função recursiva
-def contagem_regresiva(numero):
+def contagem_regressiva(numero):
     print(numero)
     if numero > 0:
         numero = numero - 1
-        contagem_regresiva(numero)
+        contagem_regressiva(numero)
     else:
         print("A contagem regressiva terminou")
 
 
-numero = int(input("Por onde a contagem regresiva deve iniciar? "))
-contagem_regresiva(10)
+numero = int(input("Por onde a contagem regressiva deve iniciar? "))
+contagem_regressiva(numero)
